6ca10f4ebec4ebfa966723df23458d0365e7962e
portgas <chelsealimbong1206@gmail.com>
2026-09-14 20:30:45 +0700

    tambah contoh struktur program Java

---
+++ src/pbo/limbong/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo.limbong;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // main() ini adalah pintu masuk program Java
+        System.out.println("Hello, World!");
+    }
+}
+
+// semua kode di Java itu wajib ada di dalam class. Eksekusi program selalu dimulai dari si method main()
